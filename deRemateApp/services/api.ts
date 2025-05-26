e4d66f45af39b35ed97e722e1e438989d0e547b0
Facundo Martín Mayordomo <mayor.facu.m@gmail.com>
2025-05-25 23:23:38 -0300

    login works
    login works

--- deRemateApp/services/api.ts
+++ deRemateApp/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from 'axios';
 import { getToken } from '../utils/auth'; 
 
-const TU_BACKEND_URL = 'http://192.168.1.100:8080'; // Esto hay que reemplazarlo
+const TU_BACKEND_URL = 'http://192.168.0.106:8080'; // Esto hay que reemplazarlo
 
 
 interface LoginResponseDTO {
