2b46324a8877a47e5c7de2b0ba8b18539d0a86b4
Facundo Martín Mayordomo <mayor.facu.m@gmail.com>
2025-05-28 00:58:03 -0300

    Paso el proyecto totalmente a Javascript
    Elimino todo lo relacionado con Typescript

--- deRemateApp/app/(tabs)/_layout.jsx
+++ deRemateApp/app/(tabs)/_layout.jsx
@@ -20,7 +20,6 @@ export default function TabLayout() {
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
           },
           default: {},
@@ -42,4 +41,4 @@ export default function TabLayout() {
       />
     </Tabs>
   );
-}
+} 
